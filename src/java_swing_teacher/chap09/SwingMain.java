e9a089423cbdfda02c96577b19d4b3f6a4484d51
teacher <net94.teacher@gmail.com>
2020-01-07 15:20:10 +0900

    예제 9-2 추가

--- src/java_swing_teacher/chap09/SwingMain.java
+++ src/java_swing_teacher/chap09/SwingMain.java
@@ -1,10 +1,20 @@
 package java_swing_teacher.chap09;
 
+import java.awt.EventQueue;
+
 public class SwingMain {
 
 	public static void main(String[] args) {
-		MyFrame mf = new MyFrame();
-		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ContentPaneEx frame = new ContentPaneEx();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
 }
